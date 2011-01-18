79d70f73b02696d747d8b601370ff62e333b0249
Thibaut DIRLIK <thibaut.dirlik@gmail.com>
2011-01-18 11:58:37 +0100

    Seems to work properly. Still miss invoice generation for rentals.

--- rent.py
+++ rent.py
@@ -18,6 +18,7 @@
 #
 
 import datetime
+import decimal_precision as dp
 
 from osv import osv, fields
 from tools.translate import _
@@ -75,9 +76,43 @@ def convert_datetimes(*args):
 class SaleOrder(osv.osv):
 
     """
-    We replace the sale order to add the possibility to switch between sellign and renting.
+    We replace the sale order to add the possibility to switch between selling and renting.
     The order header will be the same, only order lines will change.
     """
+
+    def _get_order(self, cr, uid, ids, context=None):
+        result = {}
+        for line in self.pool.get('rent.order.line').browse(cr, uid, ids, context=context):
+            result[line.order_id.id] = True
+        return result.keys()
+
+    def _amount_all(self, cr, uid, ids, field_name, arg, context=None):
+
+        """
+        This method has been overriden from sale.order to handle rent order lines.
+        """
+
+        cur_obj = self.pool.get('res.currency')
+        res = {}
+
+        for order in self.browse(cr, uid, ids, context=context):
+
+            res[order.id] = {'amount_untaxed': 0.0, 'amount_tax': 0.0, 'amount_total': 0.0, }
+            val = val1 = 0.0
+            cur = order.pricelist_id.currency_id
+
+            # Here is the only change
+            lines = order.order_line if order.sale_type == 'sale' else order.rent_order_lines
+
+            for line in lines:
+                val1 += line.price_subtotal
+                val += self._amount_line_tax(cr, uid, line, context=context)
+
+            res[order.id]['amount_tax'] = cur_obj.round(cr, uid, cur, val)
+            res[order.id]['amount_untaxed'] = cur_obj.round(cr, uid, cur, val1)
+            res[order.id]['amount_total'] = res[order.id]['amount_untaxed'] + res[order.id]['amount_tax']
+
+        return res
 
     _name = 'sale.order'
     _inherit = 'sale.order'
@@ -85,6 +120,24 @@ class SaleOrder(osv.osv):
         'sale_type' : fields.selection(TYPES, _('Order type'), required=True),
         'rent_order_lines': fields.one2many('rent.order.line', 'order_id', 'Rent Order Lines',
             readonly=True, states={'draft': [('readonly', False)]}),
+        'amount_untaxed': fields.function(_amount_all, method=True, digits_compute= dp.get_precision('Sale Price'), string='Untaxed Amount',
+            store = {
+                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['rent_order_lines'], 10),
+                'rent.order.line': (_get_order, ['price_unit', 'tax_id', 'discount', 'product_uom_qty'], 10),
+            },
+            multi='sums', help="The amount without tax."),
+        'amount_tax': fields.function(_amount_all, method=True, digits_compute= dp.get_precision('Sale Price'), string='Taxes',
+            store = {
+                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['rent_order_lines'], 10),
+                'rent.order.line': (_get_order, ['price_unit', 'tax_id', 'discount', 'product_uom_qty'], 10),
+            },
+            multi='sums', help="The tax amount."),
+        'amount_total': fields.function(_amount_all, method=True, digits_compute= dp.get_precision('Sale Price'), string='Total',
+            store = {
+                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['rent_order_lines'], 10),
+                'rent.order.line': (_get_order, ['price_unit', 'tax_id', 'discount', 'product_uom_qty'], 10),
+            },
+            multi='sums', help="The total amount."),
     }
     _defaults = {
         'sale_type' : 'sale',
@@ -130,7 +183,8 @@ class RentOrderLine(osv.osv):
 
         """
         Note that the unit price in the case of a rent is not the price of the product
-        but the price for 'one' of the selected duration unity (i.e 1 Month)
+        but the price for the selected duration (i.e 2 Month). This price will be multiplied
+        by the number of product to get the line total.
         """
 
         lines = self.browse(cursor, user_id, ids, context=context)
@@ -148,35 +202,6 @@ class RentOrderLine(osv.osv):
 
         return prices
 
-    def _get_line_price(self, cr, uid, ids, field_name, arg, context=None):
-
-        """
-        In the case of a rent line, the price depends of the duration, the number of product and the price per duration.
-
-        Ex: A product cost 1€/Month, 100 product are rented, 100€/Month, for 3 months, which does a total
-        of 300€ for 3 months.
-
-        The duration unity must be converted too: if it is defined in days in the product, and we rent
-        for a month/year, we have to convert the price per day in price per month.
-
-        To be the more accurate, with use the dateutil module to calculate exactly how many days there are between dates.
-        """
-
-        tax_obj = self.pool.get('account.tax')
-        cur_obj = self.pool.get('res.currency')
-        res = {}
-
-        if context is None:
-            context = {}
-
-        for line in self.browse(cr, uid, ids, context=context):
-            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            taxes = tax_obj.compute_all(cr, uid, line.tax_id, price, line.product_uom_qty, line.order_id.partner_invoice_id.id, line.product_id, line.order_id.partner_id)
-            cur = line.order_id.pricelist_id.currency_id
-            res[line.id] = cur_obj.round(cr, uid, cur, taxes['total'])
-
-        return res
-
     _name ='rent.order.line'
     _inherit = 'sale.order.line'
     _defaults = {
@@ -189,7 +214,6 @@ class RentOrderLine(osv.osv):
         'duration_value' : fields.integer(_('Duration'), required=True),
         'duration_unity' : fields.selection(_get_duration_unities, _('Duration unity'), required=True),
         'price_unit' : fields.function(_get_unit_price, type="float", string=_('Unit price'), method=True),
-        'price_subtotal' : fields.function(_get_line_price, type="float", string=_('Subtotal'), method=True),
     }
 
 SaleOrder(), RentOrderLine()
