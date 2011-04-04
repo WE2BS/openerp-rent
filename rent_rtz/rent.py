cd2b0dcf8c4015da46c5c9f55321e7018fc0a855
Thibaut DIRLIK <thibaut.dirlik@gmail.com>
2011-04-04 10:00:55 +0200

    Added a default 'coeff' value based on the order duration.

--- rent_rtz/rent.py
+++ rent_rtz/rent.py
@@ -20,6 +20,40 @@
 from osv import osv, fields
 from tools.translate import _
 
+COEFF_MAPPING = {
+    1 : 1,
+    2 : 1.5,
+    3 : 2,
+    4 : 2.3,
+    5 : 2.5,
+    6 : 3,
+    7 : 3.5,
+    8 : 3.3,
+    9 : 3.8,
+    10 : 4.1,
+    11 : 4.4,
+    12 : 4.,
+    13 : 4.8,
+    14 : 5,
+    15 : 5.2,
+    16 : 5.5,
+    17 : 5.7,
+    18 : 6,
+    19 : 6.2,
+    20 : 6.5,
+    21 : 6.8,
+    22 : 7,
+    23 : 7.2,
+    24 : 7.5,
+    25 : 7.8,
+    26 : 8,
+    27 : 8.2,
+    28 : 8.4,
+    29 : 8.6,
+    30 : 8.8,
+    'more' : 9,
+}
+
 class RentOrderRtzLine(osv.osv):
 
     # Inherit the rent.order.line object to add a special "Coefficient" field.
@@ -31,6 +65,16 @@ class RentOrderRtzLine(osv.osv):
             return 0.0
         return line.unit_price * product_price_factor * line.coeff
 
+    def get_default_coeff(self, cursor, user_id, context=None):
+        if context is None:
+            context = {}
+        if not 'duration' in context:
+            return 1
+        else:
+            if context['duration'] in COEFF_MAPPING:
+                return COEFF_MAPPING[context['duration']]
+        return COEFF_MAPPING['more']
+
     _inherit = 'rent.order.line'
     _name = 'rent.order.line'
     
@@ -39,7 +83,7 @@ class RentOrderRtzLine(osv.osv):
     }
     
     _defaults = {
-        'coeff' : 1.0,
+        'coeff' : get_default_coeff,
     }
 
 RentOrderRtzLine()
